3658b27514c29c34b61e39aab38c6f4b2fa30c14
Jonathan Allen <jonooallen@gmail.com>
2023-03-01 12:43:08 +1000

    Adding deleting to the database api functionality

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,7 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-def create_app():
+def create_app(config_overrides=None):
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite"
+    if config_overrides:
+        app.config.update(config_overrides)
+
+    from todo.models import db
+    from todo.models.todo import Todo
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+        db.session.commit()
 
     from .views.routes import api
     app.register_blueprint(api)
